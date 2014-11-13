4b384f92209c3c5573ee223c2444a187ea7d92ae
mattn <mattn.jp@gmail.com>
2014-11-13 22:02:45 +0900

    Fix build script

--- build.sh
+++ build.sh
@@ -2,7 +2,6 @@
 
 #[ ! -d build ] && mkdir build && cd build && cmake .. || cd build && make
 if [ ! -d build ]; then
-	echo 2
 	mkdir build && cd build && cmake .. && make
 else
     cd build && make
