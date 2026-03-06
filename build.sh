f843caf214d32c3b1bb243852e85c8c5f32ee274
Yasuhiro Matsumoto <mattn.jp@gmail.com>
2026-03-06 17:43:44 +0900

    Retab

--- build.sh
+++ build.sh
@@ -2,7 +2,7 @@
 
 #[ ! -d build ] && mkdir build && cd build && cmake .. || cd build && make
 if [ ! -d build ]; then
-	mkdir build && cd build && cmake .. && make
+    mkdir build && cd build && cmake .. && make
 else
     cd build && make
 fi
